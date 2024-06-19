ebb3d09a51f77724bbe5e494c2350c4c37064fb1
Yash Bakliwal <96653815+yashbakliwal01@users.noreply.github.com>
2024-06-19 18:01:04 +0530

    Updated entity test case

--- src/test/java/com/example/FoodApp/entity/FoodTest.java
+++ src/test/java/com/example/FoodApp/entity/FoodTest.java
@@ -1,5 +1,24 @@
 package com.example.FoodApp.entity;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class FoodTest {
 
+	@Test
+	public void testEntity() {
+		Long id = 10L;
+		String name = "Maggie";
+		String description = "Prepared in 2 minutes";
+		
+		Food food = new Food();
+		food.setId(id);
+		food.setName(name);
+		food.setDescription(description);
+		
+		Assertions.assertEquals(id, food.getId());
+        Assertions.assertEquals(name, food.getName());
+        Assertions.assertEquals(description, food.getDescription());
+	}
+	
 }
